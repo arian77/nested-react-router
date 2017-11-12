8a9184786225d22f06c6ff9aa3bec6c31af6e6d0
Arian <arianmarkus77@yahoo.com>
2017-11-12 22:35:47 +0700

    Create app.js and webpack.config.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => {
+    return (
+        <h2>{'Welcome to React Nested Routing App'}</h2>
+    );
+};
+
+render((
+    <App />
+    ),document.getElementById('app'))
